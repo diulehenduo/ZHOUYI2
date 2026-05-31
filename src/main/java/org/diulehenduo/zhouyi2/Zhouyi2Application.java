7338f6cfb5769bde15a946f9bb22c46e2d15afa4
diulehenduo <diulehenduo@diulehenduodeMacBook-Air.local>
2026-05-31 16:26:49 +0800

    test

---
+++ src/main/java/org/diulehenduo/zhouyi2/Zhouyi2Application.java
@@ -0,0 +1,15 @@
+package org.diulehenduo.zhouyi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zhouyi2Application {
+
+    public static void main(String[] args) {
+        System.out.println("server starting!!");
+        SpringApplication.run(Zhouyi2Application.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/diulehenduo/zhouyi2/Zhouyi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diulehenduo.zhouyi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zhouyi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
